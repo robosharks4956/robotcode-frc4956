e632e650a0196637fa3b3a2dbb5b192b6edf5732
calebmauer <1521403+calebmauer@users.noreply.github.com>
2023-03-04 14:45:34 -0500

    Change to VictorSPX

--- 2023/swerve2/src/main/java/frc/robot/commands/DistanceDrive.java
+++ 2023/swerve2/src/main/java/frc/robot/commands/DistanceDrive.java
@@ -1,39 +1,36 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
-public class DistanceDrive extends CommandBase
-{
+public class DistanceDrive extends CommandBase {
   public Drivetrain drive;
   public double targetXDistance;
   public double targetYDistance;
 
   /** Creates a new DistanceDrive. */
   public DistanceDrive(Drivetrain drive, double targetXDistance, double targetYDistance) {
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drive);
-  
-  this.drive = drive;
-  this.targetXDistance = targetXDistance;
-  this.targetYDistance = targetYDistance;
+
+    this.drive = drive;
+    this.targetXDistance = targetXDistance;
+    this.targetYDistance = targetYDistance;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
--- 2023/swerve2/src/main/java/frc/robot/subsystems/Slider.java
+++ 2023/swerve2/src/main/java/frc/robot/subsystems/Slider.java
@@ -1,17 +1,14 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Slider extends SubsystemBase {
-  TalonSRX motor=new TalonSRX(0);
+  VictorSPX motor=new VictorSPX(1);
   public void set(double Power){
-    motor.set(TalonSRXControlMode.PercentOutput, Power);
+    motor.set(VictorSPXControlMode.PercentOutput, Power);
 
   }
   /** Creates a new Slider. */
