552bfc9fff3e612cdc1ea992e5b75092c54e6bb2
calebmauer <1521403+calebmauer@users.noreply.github.com>
2023-03-04 10:48:52 -0500

    Fix indentation in several classes
    Drivetrain was formatted for 8 space indentation, and several other files were at 4. Probably because they were from swerve examples. All are set to 2 spaces now.

--- 2023/swerve2/src/main/java/frc/robot/commands/AutoBalance.java
+++ 2023/swerve2/src/main/java/frc/robot/commands/AutoBalance.java
@@ -13,7 +13,7 @@ import frc.robot.subsystems.Drivetrain;
 public class AutoBalance extends CommandBase {
   /** Creates a new AutoBalance. */
   public Drivetrain drive;
-  
+
   public AutoBalance(Drivetrain drive) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drive);
@@ -22,59 +22,59 @@ public class AutoBalance extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   static final double kOffBalanceAngleThresholdDegrees = 2;
-    static final double kOonBalanceAngleThresholdDegrees = 5;
+  static final double kOonBalanceAngleThresholdDegrees = 5;
   boolean autoBalanceXMode = false;
-    boolean autoBalanceYMode = true;
+  boolean autoBalanceYMode = true;
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     double xAxisRate = 1;
-        double yAxisRate = 1;
-        double pitchAngleDegrees = drive.getPitch();
-        double rollAngleDegrees = drive.getRoll();
-        SmartDashboard.putNumber("Pitch Angle", pitchAngleDegrees);
-        SmartDashboard.putNumber("Roll Angle", rollAngleDegrees);
-        
-        if (!autoBalanceXMode && (Math.abs(pitchAngleDegrees) >= Math.abs(kOffBalanceAngleThresholdDegrees))) {
-            autoBalanceXMode = true;
-        } else if (autoBalanceXMode && (Math.abs(pitchAngleDegrees) <= Math.abs(kOonBalanceAngleThresholdDegrees))) {
-            autoBalanceXMode = false;
-        }
-        if (!autoBalanceYMode && (Math.abs(pitchAngleDegrees) >= Math.abs(kOffBalanceAngleThresholdDegrees))) {
-            autoBalanceYMode = true;
-        } else if (autoBalanceYMode && (Math.abs(pitchAngleDegrees) <= Math.abs(kOonBalanceAngleThresholdDegrees))) {
-            autoBalanceYMode = false;
-        }
-        if (autoBalanceXMode) {
-          double pitchAngleRadians = pitchAngleDegrees * (Math.PI / 180.0);
-          xAxisRate = Math.sin(pitchAngleRadians) * -1;
-      }
-      if (autoBalanceYMode) {
-          double rollAngleRadians = rollAngleDegrees * (Math.PI / 180.0);
-          yAxisRate = Math.sin(rollAngleRadians) * -1;
-      }
+    double yAxisRate = 1;
+    double pitchAngleDegrees = drive.getPitch();
+    double rollAngleDegrees = drive.getRoll();
+    SmartDashboard.putNumber("Pitch Angle", pitchAngleDegrees);
+    SmartDashboard.putNumber("Roll Angle", rollAngleDegrees);
 
-      try {
-        drive.drive(
-          new ChassisSpeeds(
-             xAxisRate*40,
-             yAxisRate*40,
-             0    
-          )
-  );
-      } catch (RuntimeException ex) {
-          String err_string = "Drive system error:  " + ex.getMessage();
-          DriverStation.reportError(err_string, true);
-      }
+    if (!autoBalanceXMode && (Math.abs(pitchAngleDegrees) >= Math.abs(kOffBalanceAngleThresholdDegrees))) {
+      autoBalanceXMode = true;
+    } else if (autoBalanceXMode && (Math.abs(pitchAngleDegrees) <= Math.abs(kOonBalanceAngleThresholdDegrees))) {
+      autoBalanceXMode = false;
     }
+    if (!autoBalanceYMode && (Math.abs(pitchAngleDegrees) >= Math.abs(kOffBalanceAngleThresholdDegrees))) {
+      autoBalanceYMode = true;
+    } else if (autoBalanceYMode && (Math.abs(pitchAngleDegrees) <= Math.abs(kOonBalanceAngleThresholdDegrees))) {
+      autoBalanceYMode = false;
+    }
+    if (autoBalanceXMode) {
+      double pitchAngleRadians = pitchAngleDegrees * (Math.PI / 180.0);
+      xAxisRate = Math.sin(pitchAngleRadians) * -1;
+    }
+    if (autoBalanceYMode) {
+      double rollAngleRadians = rollAngleDegrees * (Math.PI / 180.0);
+      yAxisRate = Math.sin(rollAngleRadians) * -1;
+    }
+
+    try {
+      drive.drive(
+          new ChassisSpeeds(
+              xAxisRate * 40,
+              yAxisRate * 40,
+              0));
+    } catch (RuntimeException ex) {
+      String err_string = "Drive system error:  " + ex.getMessage();
+      DriverStation.reportError(err_string, true);
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
